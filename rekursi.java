8a4be5152bde0a41a56df76d66008ce735de9d7f
wafaaidan <wafa.aidan@gmail.com>
2025-12-30 13:54:04 +0700

    test

---
+++ rekursi.java
@@ -0,0 +1,15 @@
+public class rekursi {
+    public int Factorial(int n) {
+        if (n>0) {
+            return n * Factorial(n - 1);
+        } else {
+            return 1;
+        }
+    }
+    public int BagiRuang() {}
+    public static void main(String[] args) {
+        rekursi obj = new rekursi();
+        int factorial = obj.Factorial(5); 
+        System.out.println(factorial);
+    }
+}
